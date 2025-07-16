90307543384463ee5d73b366367000e68f4874e1
OmarAboushousha <oaboushousha99@gmail.com>
2025-07-16 12:49:23 -0400

    added login and authentication related functions for auth_users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,21 +7,56 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+  let validUsernames = users.filter((user) => user.username === username);
+  if (validUsernames.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+  let authUsers = users.filter((user) => user.username === username && user.password === password);
+  if (authUsers.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  const validUsername = isValid(username);
+  if (!username || !password) {
+    return res.status(404).send("Error logging in");
+  }
+  if (!validUsername) {
+    return res.status(404).send("Username does not exist");
+  }
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+        data: password
+    }, "access", {expiresIn: 60 * 60});
+    req.session.authorization = {
+        accessToken, username
+    }
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).send("Error logging in. Check username and password");
+  }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
+  const id = req.params.isbn;
+  const username = req.session.authorization.username;
+  const review = req.query.review;
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
